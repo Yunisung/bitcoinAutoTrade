21478d2cb24b92b0165952611bc17944307470d5
yunisung <pys0102@gmail.com>
2024-10-14 23:02:59 +0900

    모르것다

--- mvp.py
+++ mvp.py
@@ -1,11 +1,14 @@
 import telegram
 import asyncio
 import os
+import pyupbit
 from dotenv import load_dotenv
 load_dotenv()
 
 access = os.getenv("UPBIT_ACCESS_KEY")
 secret = os.getenv("UPBIT_SECRET_KEY")
+upbit = pyupbit.Upbit(access, secret)
+
 
 async def send_message(message):
     #텔레그램 메세지 보내기
@@ -31,8 +34,9 @@ asyncio.run(send_message("ai_trading start"))
 def ai_trading():
     
     # 1. 업비트 차트 가져오기 (30일 일봉)
-    import pyupbit
+    
     df = pyupbit.get_ohlcv("KRW-BTC", count=30, interval="day");
+    # print(df)
 
     # 2. AI에게 데이터 제공하고 판단 받기
     from openai import OpenAI
@@ -74,25 +78,41 @@ def ai_trading():
     asyncio.run(send_message(result["decision"].upper()))
     asyncio.run(send_message(result["reason"]))
 
-    upbit = pyupbit.Upbit(access, secret)
-
-    
+    # print(result["decision"].upper())
+    # print(result["reason"])
+    # my_btc = get_balance("BTC")
+    # current_price = pyupbit.get_orderbook(ticker="KRW-BTC")['orderbook_units'][0]["ask_price"]
+    # print("현재 1비트 금액 : ", current_price)
+    # print("현재 보유 코인 : ", my_btc)
+    # print("합계 : ", my_btc * current_price)
+    # my_krw = get_balance("KRW")
+    # print("현재 현금 : ", my_krw)
 
     if result["decision"] == "buy":
-        my_krw = upbit.get_balances("KRW")
+        my_krw = get_balance("KRW")
+        buy_cost = my_krw * 0.9995
+        # print("현재 현금 : ", buy_cost)
         if my_krw * 0.9995 > 5000:
+            # print("구매결정")
             buy_result = upbit.buy_market_order("KRW-BTC", my_krw * 0.9995)
             asyncio.run(send_message(buy_result))
         else:
             asyncio.run(send_message("구입실패 : 5000원 미만"))
+            print("구입실패 : 5000원 미만")
     elif result["decision"] == "sell":
-        my_btc = upbit.get_balances("KRW-BTC")
+        my_btc = get_balance("BTC")
         current_price = pyupbit.get_orderbook(ticker="KRW-BTC")['orderbook_units'][0]["ask_price"]
+        # print("현재 1비트 금액  : ", current_price)
+        # print("현재 재산 : ", my_btc * current_price)
         if my_btc * current_price > 5000:
+            # print("판매결정")
             sell_result = upbit.sell_market_order("KRW-BTC", my_btc)
             asyncio.run(send_message(sell_result))
         else:
-            asyncio.run(send_message("판매실패 : 5000원 미만"))        
+            asyncio.run(send_message("판매실패 : 5000원 미만"))
+            print("판매실패 : 5000원 미만")        
+
+# ai_trading()
 
 while True:
     import time
